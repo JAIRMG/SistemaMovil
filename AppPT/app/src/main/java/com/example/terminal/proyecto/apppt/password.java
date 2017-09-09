89083bef9a4f20056dd721662b2ee0473da27249
JAIRMG <jair.mg.27@gmail.com>
2017-09-09 18:43:53 -0500

    Consume web service para login, registro, actulizar pass y olvide contrasenia

--- AppPT/app/src/main/java/com/example/terminal/proyecto/apppt/password.java
+++ AppPT/app/src/main/java/com/example/terminal/proyecto/apppt/password.java
@@ -11,19 +11,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class password extends AppCompatActivity {
+
+    public static final String FINAL_URL = "http://192.168.1.74:8084/WebServiceForApp/webresources/generic";
+    public static final String SIGN_IN = "/forgotPassword/";
+    public static final String ERROR_FROM_NETWORK_NOT_CONNECTED = "Error, Conexión a internet no disponible";
 
     public Context context;
     Button botonEnviar;
     EditText campoCorreo;
     String correo="";
 
-
-    public void sendToLogin(View view)
-    {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,13 +43,22 @@ public class password extends AppCompatActivity {
 
                 correo = campoCorreo.getText().toString();
 
-                if(!correo.equals("") && correo.contains("@")){
+                if(!correo.equals("") && correo.contains("@") && correo.contains(".")){
 
 
+                    String finalJson = "";
 
+                    JSONObject userRequest = new JSONObject();
+                    try{
+                        userRequest.put("email", correo);
 
-                    Intent intent = new Intent(context, MainActivity.class);
-                    startActivity(intent);
+                        finalJson = userRequest.toString();
+                    } catch(JSONException ex){
+                        System.out.println("Error al crear JSON: "+ex);
+                    }
+
+                    new recoverPassword().execute(finalJson);
+
 
                 } else {
                     Toast.makeText(context, "Verifica que el correo sea correcto",
@@ -58,4 +69,76 @@ public class password extends AppCompatActivity {
         });
 
     }
+
+    private class recoverPassword extends AsyncTask<String, Long, String> {
+
+
+        protected String doInBackground(String... data) {
+
+
+            String jsonLogin = data[0].replace("{","%7B").replace("}","%7D");
+            jsonLogin = "["+jsonLogin+"]";
+            Log.i("jsonFinal",jsonLogin);
+            return HttpRequest.sendGetRequest(FINAL_URL + SIGN_IN + jsonLogin);
+        }
+
+        protected void onPostExecute(String response) {
+
+            if(response==null || response.equals("")) {
+                Toast.makeText(getApplicationContext(), "Error en el servidor, Inténtalo más tarde", Toast.LENGTH_SHORT).show();
+
+            }
+            else if(response.equals(ERROR_FROM_NETWORK_NOT_CONNECTED)) {
+                Toast.makeText(getApplicationContext(), "ERROR_FROM_NETWORK_NOT_CONNECTED", Toast.LENGTH_SHORT).show();
+
+            }
+            else if(response.contains("Error") && response.contains("Message")) {
+                try {
+                    JSONObject json = new JSONObject(response);
+                    String message = json.getString("Message");
+                    Toast.makeText(getApplicationContext(), "error: "+message, Toast.LENGTH_SHORT).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            else{ //if(JSONBuilder.checkJsonStructure(response)) {
+                Log.i("MyTAG: ", response);
+                CheckIfCorrect(response);
+
+            }
+
+        }
+
+        private void CheckIfCorrect(String response){
+
+            String respuesta = "";
+
+            try{
+                JSONArray jsonarray = new JSONArray(response);
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    JSONObject jsonobject = jsonarray.getJSONObject(i);
+                    respuesta = jsonobject.getString("respuesta");
+                }
+            } catch(JSONException ex){
+                System.out.println("Error al parsear json "+ ex);
+            }
+            Log.i("Respuesta: ", response);
+
+            if(respuesta.equals("1")){
+                Toast.makeText(getApplicationContext(), "Se ha enviado un correo con tu nueva contraseña", Toast.LENGTH_SHORT).show();
+                goHome();
+            } else{
+                Toast.makeText(getApplicationContext(), "Correo inválido", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }
+    }
+
+    public void goHome(){
+        Intent intent = new Intent(context, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
